c1a129913fc064da042ea3c872f72998d5055e2c
devesh <deveshpatil9000@gmail.com>
2025-01-14 01:08:07 +0530

    Handling posts

--- Projects/Social-Media/src/components/CreatePost.jsx
+++ Projects/Social-Media/src/components/CreatePost.jsx
@@ -1,42 +1,69 @@
 const CreatePost = () => {
   return (
-    <form>
+    <form className="create-post">
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="User-Id" className="form-label">
+          Enter User-Id
         </label>
         <input
-          type="email"
+          type="text"
           className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
+          id="User-Id"
+          placeholder="Your user Id"
         />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
+
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
+        <label htmlFor="title" className="form-label">
+          Enter post title
         </label>
         <input
-          type="password"
+          type="text"
           className="form-control"
-          id="exampleInputPassword1"
+          id="title"
+          placeholder="How are you feeling today"
         />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post content
         </label>
+        <textarea
+          rows={4}
+          type="text"
+          className="form-control"
+          id="body"
+          placeholder="Tell us more about it"
+        />
       </div>
+
+      <div className="mb-3">
+        <label htmlFor="reaction" className="form-label">
+          No.of reactions
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="reaction"
+          placeholder="How many people reacted to this post"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your hashtags
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="tags"
+          placeholder="Tag the post"
+        />
+      </div>
+
       <button type="submit" className="btn btn-primary">
-        Submit
+        Post
       </button>
     </form>
   );
--- Projects/Social-Media/src/store/post-list-store.jsx
+++ Projects/Social-Media/src/store/post-list-store.jsx
@@ -1,46 +1,62 @@
 import { createContext, useReducer } from "react";
 
-const PostList = createContext ({
-    postList : [] ,
-    addPost : () => {} ,                    //All this three are the required default structure in our post list using createPost
-    deletePost : () => {} ,
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {}, //All this three are the required default structure in our post list using createPost
+  deletePost: () => {},
 });
 
 const postListReducer = (currPostList, action) => {
-    return currPostList;
-}
 
-const PostListProvider = ({children}) => {
+    let newPostList = currPostList;
+    if(action.type === "DELETE_POST"){
+        newPostList = currPostList.filter((post) => post.id !== action.payload.postId)
+    }
 
-    const[postList, dispatchPostList] = useReducer(postListReducer , DEFAULT_POST_LIST);
+  return newPostList;
+};
 
-    const addPost = () => {};
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
 
-    const deletePost = () => {};
+  const addPost = () => {};
 
-    return(
-        <PostList.Provider value={{postList, addPost, deletePost}}>{children}</PostList.Provider> //object having same key value pair can be written single
+  const deletePost = (postId) => {
+    dispatchPostList({
+        type: "DELETE_POST" ,
+        payload:{
+            postId,
+        }
+    })
+  };
 
-    )
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider> //object having same key value pair can be written single
+  );
 };
 
 const DEFAULT_POST_LIST = [
-    {
-        id: '1',
-        title: "going to mumbai",
-        body: "Hii friends ,I am going to mumbai for my vacation, hope enjoy a lot",
-        reaction: 2,
-        userId: "user-9",
-        tags:["vacation","mumbai","enjoy"]
-    },
-    {
-        id: '2',
-        title: "Pass zalo bhai",
-        body: "4 saal ke mehenate ke badd ,pass hoo gaye bhai Unbelievable",
-        reaction: 4,
-        userId: "user-10",
-        tags:["4saal","pass","BTech"]
-    }
-]
+  {
+    id: "1",
+    title: "going to mumbai",
+    body: "Hii friends ,I am going to mumbai for my vacation, hope enjoy a lot",
+    reaction: 2,
+    userId: "user-9",
+    tags: ["vacation", "mumbai", "enjoy"],
+  },
+  {
+    id: "2",
+    title: "Pass zalo bhai",
+    body: "4 saal ke mehenate ke badd ,pass hoo gaye bhai Unbelievable",
+    reaction: 4,
+    userId: "user-10",
+    tags: ["4saal", "pass", "BTech"],
+  },
+];
 
 export default PostListProvider;
