841c65dc373238e97d192b6e1e0047a245c4d51b
devesh <deveshpatil9000@gmail.com>
2025-01-15 13:36:36 +0530

    work with dummy API

---
+++ 5-Advance-react/Social-Media copy/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+const WelcomeMessage = ({onGetPostsClick}) => {
+  return (
+    <center className="welcomeMEssage">
+      <h1>There are no posts</h1>
+      <button type="button" className="btn btn-primary" onClick={onGetPostsClick}>
+        Get posts from server
+      </button>
+    </center>
+  );
+};
+
+export default WelcomeMessage;
