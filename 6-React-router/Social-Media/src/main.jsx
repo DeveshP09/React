363ba09562983cf5f099b69e43ce53e1cfd6e6ca
devesh <deveshpatil9000@gmail.com>
2025-01-24 23:18:57 +0530

    React Router , Route links and useNavigate hook

---
+++ 6-React-router/Social-Media/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./routes/App.jsx";
+import CreatePost from "./components/CreatePost.jsx";
+import PostList from "./components/PostList.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <PostList /> },
+      { path: "/create-post", element: <CreatePost /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
