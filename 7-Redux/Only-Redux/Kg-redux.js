c777e12abdf8afb59d7baa65f032a9d6723d7bc7
devesh <deveshpatil9000@gmail.com>
2025-01-25 23:34:16 +0530

    Redux

---
+++ 7-Redux/Only-Redux/Kg-redux.js
@@ -0,0 +1,12 @@
+const redux = require('redux') ;
+
+const reducer = (store, action) => {
+    return store;
+}
+
+const store = redux.createStore(reducer);
+
+const subscriber = () => {
+    const state = store.getState();
+    console.log(state);
+}
