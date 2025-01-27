bac8e598261696ae244f3985b1ca8844caef6b38
devesh <deveshpatil9000@gmail.com>
2025-01-27 15:51:20 +0530

    update

--- 7-Redux/Only-Redux/Kg-redux.js
+++ 7-Redux/Only-Redux/Kg-redux.js
@@ -1,7 +1,19 @@
 const redux = require('redux') ;
 
-const reducer = (store, action) => {
-    return store;
+const INITIAL_VALUE = {
+counter : 0 ,
+};
+
+const reducer = (store = INITIAL_VALUE, action) => {
+
+    let newStore = store;
+    if(action.type === 'Increment'){
+        newStore = {counter :store.counter + 1};
+    }
+    else if(action.type ==='Addition'){
+        newStore = {counter : store.counter + action.payload.number}
+    }
+    return newStore;
 }
 
 const store = redux.createStore(reducer);
@@ -10,3 +22,8 @@ const subscriber = () => {
     const state = store.getState();
     console.log(state);
 }
+
+store.subscribe(subscriber);
+store.dispatch({type : 'Increment'});
+store.dispatch({type : 'Increment'});
+store.dispatch({type : 'Addition' ,payload : {number : 8}});
